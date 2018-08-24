28c2b46a0e70e775f3e9b6d600686222783704e0
Bayu Syahrian <bs503711@gmail.com>
2018-08-24 09:39:11 +0700

    komitketiga

--- app/src/main/java/com/kelompok8/android/kedua.java
+++ app/src/main/java/com/kelompok8/android/kedua.java
@@ -4,5 +4,5 @@ package com.kelompok8.android;
  * Created by LABKOM2-RPL 7 on 24/08/2018.
  */
 
-public class kedua {System.out.println("zahranganteng ea");
+public class kedua {System.out.println("zahran ganteng pisan ea");
 }
