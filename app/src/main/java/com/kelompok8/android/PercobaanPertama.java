3deee9237a6c9e2250a430db46a67a738260d9dc
Bayu Syahrian <bayusyahrianxrpla@gmail.com>
2018-08-24 08:59:48 +0700

    First

---
+++ app/src/main/java/com/kelompok8/android/PercobaanPertama.java
@@ -0,0 +1,13 @@
+package com.kelompok8.android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PercobaanPertama extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_percobaan_pertama);
+    }
+}
